a5678e11d49da7c74591f245eab94c996fb29854
Lopez Oderda, Lucas F <Lucas.F.Lopez.Oderda@kcc.com>
2018-05-20 11:32:20 -0300

    WishAPI Console Example + README.md

---
+++ Examples/WishAPIInventory/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WishAPIInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sku = "";
+            string productId = "";
+
+            int currentInventory = WishAPICalls.GetItemStock(productId);
+            Console.WriteLine("Current Inventory Product " + productId + " SKU " + sku + ": " + currentInventory.ToString());
+            bool updateOk = WishAPICalls.UpdateItemStock(sku, currentInventory + 10);
+            if (updateOk)
+                Console.WriteLine("SKU " + sku + " inventory updated");
+        }
+    }
+
+    class WishAPICalls
+    {
+        static string accessToken = "";
+
+        const string baseURL = "https://sandbox.merchant.wish.com/api/v2";
+
+        static HttpClient client = new HttpClient();
+
+        public static int GetItemStock(string product)
+        {
+            string URL = baseURL + "/product?id=" + product + "&access_token=" + accessToken;
+
+            var content = callURL(URL);
+
+            dynamic stuff = JsonConvert.DeserializeObject(content);
+
+            return stuff.data.Product.variants[0].Variant.inventory;
+        }
+
+        public static bool UpdateItemStock(string sku, int inventory)
+        {
+            string URL = baseURL + "/variant/update-inventory";
+
+            var dict = new Dictionary<string, string>();
+            dict.Add("sku", sku);
+            dict.Add("inventory", inventory.ToString());
+            dict.Add("access_token", accessToken);
+
+            var content = callURL(URL, HttpMethod.Post, new FormUrlEncodedContent(dict));
+
+            dynamic stuff = JsonConvert.DeserializeObject(content);
+
+            return stuff.code == "0" ;
+        }
+
+        static string callURL(string URL)
+        {
+            return callURL(URL, HttpMethod.Get, null);
+
+        }
+
+        static string callURL(string URL, HttpMethod method, HttpContent content)
+        {
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(URL),
+                Method = method
+            };
+
+            if (content != null)
+                request.Content = content;
+
+            var result = client.SendAsync(request).Result;
+
+            string jsonContent = result.Content.ReadAsStringAsync().Result;
+
+            return jsonContent;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # InventoryToMarketplace
+
+## English
 Examples update Stock from Inventory systerm to Ecommerce Marketplace
+
+## Español
+Ejemplos actualizar stock desde un sistema de inventarios a Ecommerce Marketplace
+
+#### Articulos
+[Desde un Sistema de Inventario a Ecommerce Marketplace — APIs & Cloud & ERP][1]
+
+[Como obtener el nivel de inventario desde SAP S/4HANA Cloud usando APIs][2]
+
+[Actualizando Inventario en MercadoLibre — API][3]
+
+[1]: https://medium.com/@lopezlucas/desde-un-sistema-de-inventario-a-ecommerce-marketplace-apis-cloud-erp-d1877d97f3f9
+[2]: https://medium.com/@lopezlucas/como-obtener-el-nivel-de-inventario-desde-sap-s-4hana-cloud-usando-apis-40ab76951493
+[3]: https://medium.com/@lopezlucas/actualizando-inventario-en-mercadolibre-api-be08c72cc9df
